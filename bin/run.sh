9f56e96fd5d2bc027272296cf8c27de7eaa15733
liu <liuy57@rpi.edu>
2020-09-08 22:55:19 -0400

    First commit

---
+++ bin/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Run my program.
+python3 server.py $1 
+
+exit 0
---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+rm -rf bin
+mkdir bin
+cp -R src/*  bin/
+cp run.sh bin/
+
+
+echo Done!
+
+exit 0
